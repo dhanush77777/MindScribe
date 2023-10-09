e13682a2dc2e2d5490e9d421e103ac62aa19b684
dhanush77777 <saidhanushm@gmail.com>
2023-10-09 19:40:13 +0530

    added stopwords

--- Hashtags/app.py
+++ Hashtags/app.py
@@ -2,7 +2,6 @@ from flask import Flask ,request , jsonify
 import string
 import nltk
 import re
-
 def tokenize_text(text):
     # Use regular expression to split text into words
     words = re.findall(r'\w+', text.lower())
@@ -10,14 +9,9 @@ def tokenize_text(text):
 
 # nltk.download('stopwords')
 #nltk.download('punkt')
-
-# Function to extract main context and generate hashtags using NLTK
-def extract_context_and_generate_hashtags(article_text, num_keywords=5):
-    # Tokenize the article text into words
-    words = tokenize_text(article_text.lower())
-
-    # Remove punctuation and stopwords
-    stop_words = set(["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your",
+def stop__words():
+    a=['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+    b=["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your",
                      "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she",
                      "her", "hers", "herself", "it", "its", "itself", "they", "them", "their",
                      "theirs", "themselves", "what", "which", "who", "whom", "this", "that",
@@ -26,7 +20,15 @@ def extract_context_and_generate_hashtags(article_text, num_keywords=5):
                      "the", "and", "but", "if", "or", "because", "as", "until", "while", "of",
                      "at", "by", "for", "with", "about", "against", "between", "into", "through",
                      "during", "before", "after", "above", "below", "to", "from", "up", "down",
-                     "in", "out", "on", "off", "over", "under", "again", "further", "then", "once"])
+                     "in", "out", "on", "off", "over", "under", "again", "further", "then", "once"]
+    return set(a+b)
+# Function to extract main context and generate hashtags using NLTK
+def extract_context_and_generate_hashtags(article_text, num_keywords=5):
+    # Tokenize the article text into words
+    words = tokenize_text(article_text.lower())
+
+    # Remove punctuation and stopwords
+    stop_words = stop__words()
     filtered_words = [word.strip(string.punctuation) for word in words if word.strip(string.punctuation) not in stop_words]
 
     # Calculate word frequencies
@@ -56,4 +58,4 @@ def hashtag():
                
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=int("6000"),debug=True)
