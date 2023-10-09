7fc97bcf707d46422ed9c2461c1c3181bcca9ed4
dhanush77777 <saidhanushm@gmail.com>
2023-10-09 13:41:39 +0530

    changed algo

--- Hashtags/app.py
+++ Hashtags/app.py
@@ -1,42 +1,32 @@
 from flask import Flask ,request , jsonify
-from flair.data import Sentence
-from flair.models import SequenceTagger
 import string
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
+nltk.download('stopwords')
+nltk.download('punkt')
+
+# Function to extract main context and generate hashtags using NLTK
 def extract_context_and_generate_hashtags(article_text, num_keywords=5):
-    sentence = Sentence(article_text)
+    # Tokenize the article text into words
+    words = word_tokenize(article_text.lower())
 
-    tagger = SequenceTagger.load("ner")
-    tagger.predict(sentence)
-    entities = [entity.text for entity in sentence.get_spans('ner')]
-    stopwords = set(["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your",
-                     "yours", "yourself", "yourselves", "he", "him", "his", "himself", "she",
-                     "her", "hers", "herself", "it", "its", "itself", "they", "them", "their",
-                     "theirs", "themselves", "what", "which", "who", "whom", "this", "that",
-                     "these", "those", "am", "is", "are", "was", "were", "be", "been", "being",
-                     "have", "has", "had", "having", "do", "does", "did", "doing", "a", "an",
-                     "the", "and", "but", "if", "or", "because", "as", "until", "while", "of",
-                     "at", "by", "for", "with", "about", "against", "between", "into", "through",
-                     "during", "before", "after", "above", "below", "to", "from", "up", "down",
-                     "in", "out", "on", "off", "over", "under", "again", "further", "then", "once"])
+    # Remove punctuation and stopwords
+    stop_words = set(stopwords.words('english'))
+    filtered_words = [word.strip(string.punctuation) for word in words if word.strip(string.punctuation) not in stop_words]
 
-    filtered_entities = [entity.strip(string.punctuation) for entity in entities if entity.strip(string.punctuation) not in stopwords]
+    # Calculate word frequencies
+    word_freq = nltk.FreqDist(filtered_words)
 
-    entity_freq = {}
-    for keyword in filtered_entities:
-        if keyword in entity_freq:
-            entity_freq[keyword] += 1
-        else:
-            entity_freq[keyword] = 1
+    # Get the top keywords
+    top_keywords = [word for word, freq in word_freq.most_common(num_keywords)]
 
-  
-    sorted_entities = sorted(entity_freq.items(), key=lambda x: x[1], reverse=True)
-    top_keywords = [keyword for keyword, _ in sorted_entities[:num_keywords]]
-
- 
+    # Generate hashtags from the top keywords
     hashtags = ['#' + keyword.replace(' ', '') for keyword in top_keywords]
 
     return hashtags
+
 
 
 
@@ -53,4 +43,4 @@ def hashtag():
                
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=int("3000"),debug=True)
+    app.run(debug=True)
